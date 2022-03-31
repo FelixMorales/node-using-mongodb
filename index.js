ea6c688c7393bda61f23fae6ae2c65d4b0e79399
FelixMorales <felixmorales747@gmail.com>
2022-03-31 13:27:52 -0400

    feat: connecting to mongodb

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-import express from 'express';
-import bodyParser from 'body-parser';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
 
 const app = express();
 const PORT = 4000;
 
+mongoose
+  .connect("mongodb://winhost:27017/productsdb", {useNewUrlParser: true, useUnifiedTopology: true})
+  .then((conn) => {
+    console.log('success');
+    console.log(conn)
+  })
+  .catch((e) => {
+    console.log("error");
+    console.log(e)
+  });
+
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 
-app.get('/', (req, res) =>
-    res.send(`Store server running on port ${PORT}`)
-);
+app.get("/", (req, res) => res.send(`Store server running on port ${PORT}`));
 
-app.listen(PORT, () => 
-    console.log(`Your server is running on port ${PORT}`)
-);
+app.listen(PORT, () => console.log(`Your server is running on port ${PORT}`));
